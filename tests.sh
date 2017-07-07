7be2cc32dfb733054a417680bcf6ef0f2d4a2934
Dustin Long <me@dustmop.io>
2017-07-07 00:36:01 -0400

    Tests for more instructions, the shift instructions

--- tests.sh
+++ tests.sh
@@ -4,6 +4,7 @@ racket unit-tests/error-tests.scm
 racket unit-tests/function-tests.scm
 racket unit-tests/loop-tests.scm
 racket unit-tests/math-tests.scm
+racket unit-tests/mutator-tests.scm
 racket unit-tests/memory-tests.scm
 racket unit-tests/opcode-tests.scm
 racket unit-tests/proc-tests.scm
---
+++ unit-tests/mutator-tests.scm
@@ -0,0 +1,24 @@
+#lang racket
+
+(require rackunit "../compile.scm")
+
+(define (compile-code code)
+  (clear-result)
+  (make-variable! 'n)
+  (process-form (datum->syntax #f code))
+  (fetch-result))
+
+; TODO:
+;(check-equal? (compile-code '(asl)) '("  asl a"))
+
+(check-equal? (compile-code '(asl a)) '("  asl a"))
+
+(check-equal? (compile-code '(asl n)) '("  asl n"))
+
+(check-equal? (compile-code '(inc n)) '("  inc n"))
+
+(check-equal? (compile-code '(lsr n)) '("  lsr n"))
+
+(check-equal? (compile-code '(rol n)) '("  rol n"))
+
+(check-equal? (compile-code '(ror n)) '("  ror n"))
