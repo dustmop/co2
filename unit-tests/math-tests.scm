3fe543b8a98493963f2312212ae7c1fba425a07a
Dustin Long <me@dustmop.io>
2017-07-12 13:01:44 -0400

    Multiple and divide, short forms using shifts and bitmasks

--- unit-tests/math-tests.scm
+++ unit-tests/math-tests.scm
@@ -7,7 +7,7 @@
   (process-form (datum->syntax #f code))
   (fetch-result))
 
-; TODO: Implement constant folding, which should break most of these tests.
+                                        ; TODO: Implement constant folding, which should break most of these tests.
 (check-equal? (compile-code '(+ 1 2)) '("  lda #$1"
                                         "  clc"
                                         "  adc #$2"))
@@ -20,7 +20,16 @@
                                          "  lsr a"
                                          "  lsr a"))
 
-(check-equal? (compile-code '(* 3 4)) '("  lda #$4"
+(check-equal? (compile-code '(* 3 2)) '("  lda #$3"
+                                        "  asl a"))
+
+(check-equal? (compile-code '(* 3 16)) '("  lda #$3"
+                                         "  asl a"
+                                         "  asl a"
+                                         "  asl a"
+                                         "  asl a"))
+
+(check-equal? (compile-code '(* 3 5)) '("  lda #$5"
                                         "  bne _mul_start_0001"
                                         "  lda #0"
                                         "  jmp _mul_done_0004"
@@ -40,3 +49,11 @@
                                         "  dey"
                                         "  bne _mul_loop_0002"
                                         "_mul_done_0004:"))
+
+(check-equal? (compile-code '(/ 75 8)) '("  lda #$4b"
+                                         "  lsr a"
+                                         "  lsr a"
+                                         "  lsr a"))
+
+(check-equal? (compile-code '(mod 75 8)) '("  lda #$4b"
+                                           "  and #7"))
